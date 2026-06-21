48460e54be1e7c9265eff68d6b9d79396f252f57
Fresherpb35 <ds5513063@gmail.com>
2026-06-21 10:05:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ db();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174","https://campus-hzf3.vercel.app"],
     credentials: true,
   },
 });
