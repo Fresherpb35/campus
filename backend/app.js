85513e6b1c2d464cde90401c53643cde7e5ced99
Fresherpb35 <ds5513063@gmail.com>
2026-06-21 10:05:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 //  middlewares:
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174","https://campus-hzf3.vercel.app"],
     credentials: true,
   })
 );
